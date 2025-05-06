1f0b8afd4107b091ef4ba50fa65f3c390af6e24b
Andrade <andrade.nicolas.melo@outlook.com>
2025-05-05 23:24:45 -0300

    Adicionar arquivos de projeto.

---
+++ contaCorrente.ConsoleApp1/ContaCorrente.cs
@@ -0,0 +1,75 @@
+﻿namespace contaCorrente.ConsoleApp1
+{
+    public class Movimentacao
+    {
+        public decimal valor;
+        public string tipo;
+
+        public string LerMovimentacao()
+        {
+            return $"{tipo} - {valor.ToString("C2")}";
+        }
+    }
+
+    public class ContaCorrente
+    {
+        public int id;
+        public decimal saldo;
+        public decimal limite;
+        public Movimentacao[] movimentacoes;
+        int contadorMovimentacao = 0;
+    
+        public void Depositar(decimal depositar)
+        {
+            saldo = saldo + depositar;
+
+            Movimentacao novaMovimentacao = new Movimentacao();
+            novaMovimentacao.valor = depositar;
+            novaMovimentacao.tipo = "Credito";
+            movimentacoes[contadorMovimentacao] = novaMovimentacao;
+        }
+
+        public void Sacar(decimal sacar)
+        {
+            if (sacar <= saldo + limite)
+            {
+                saldo -= sacar;
+
+                Movimentacao novaMovimentacao = new Movimentacao();
+                novaMovimentacao.valor = sacar;
+                novaMovimentacao.tipo = "Debito";
+
+                movimentacoes[contadorMovimentacao] = novaMovimentacao;
+                contadorMovimentacao++;
+            }
+
+            else
+            {
+                Console.WriteLine("Quantia solicitada ultrapassa o saldo + limite");
+            }
+        }
+
+        public void Transferencia(ContaCorrente destinatario, decimal quantia)
+        {
+            destinatario.Depositar(quantia);
+
+            this.Sacar(quantia);
+        }
+
+        public void ExibirExtrato()
+        {
+            Console.WriteLine("Extrato da Conta #" + this.id);
+            Console.WriteLine("Saldo: " + this.saldo);
+
+            for(int i = 0; i < movimentacoes.Length; i++)
+            {
+                Movimentacao movimentacaoAtual = movimentacoes[i];
+
+                if (movimentacaoAtual != null)
+                {
+                    Console.WriteLine(movimentacaoAtual.LerMovimentacao());
+                }
+            }
+        }
+    }
+}
---
+++ contaCorrente.ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace contaCorrente.ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta1 = new ContaCorrente();
+
+            conta1.id = 1;
+            conta1.saldo = 1000;
+            conta1.limite = 0;
+            conta1.movimentacoes = new Movimentacao[1000000];
+
+            conta1.Sacar(200);
+            conta1.Depositar(300);
+            conta1.Depositar(500);
+            conta1.Sacar(200);
+
+            ContaCorrente conta2 = new ContaCorrente();
+
+            conta2.id = 2;
+            conta2.saldo = 300;
+            conta2.limite = 0;
+            conta2.movimentacoes = new Movimentacao[1000000];
+
+            conta1.Transferencia(conta2, 400);
+
+            conta1.ExibirExtrato();
+
+            Console.WriteLine();
+
+            conta2.ExibirExtrato();
+        }
+    }
+}
